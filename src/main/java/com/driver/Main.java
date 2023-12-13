3e120f0493a4ae1698c550200b04ed708b20cc58
Pratik Jadhav <prateek7120@gmail.com>
2023-12-13 20:37:30 +0530

    Second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[]args)
     {
-        B b=new B();
-        b.meth();
+        B objB=new B();
+        String result=objB.meth();
+        System.out.println(result);
     }
 
 }
